82f23685894f53e242e14b0a67acbb91f46cb7bb
jdguzmans <jdguzmans@hotmail.com>
2017-09-16 15:19:08 -0500

    created a method to load automatically all routes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var fs = require('fs')
 var express = require('express')
 var path = require('path')
 // var favicon = require('serve-favicon')
@@ -6,14 +7,11 @@ var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 
 var mongoClient = require('mongodb').MongoClient
-mongoClient.connect('mongodb://node:node@ds036967.mlab.com:36967/speak-your-mind', function (err, db) {
+mongoClient.connect('mongodb://node:node@ds036967.mlab.com:36967/speak-your-mind', (err, db) => {
   if (err) console.log('Error trying to connect to DB')
   console.log('Connected to DB')
   db.close()
 })
-
-var index = require('./routes/index')
-var users = require('./routes/users')
 
 var app = express()
 
@@ -29,10 +27,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, './front/public')))
 
-app.use('/', index)
-app.use('/users', users)
+// Loads al the files in directory '.routes' and creates its route
+fs.readdirSync('./routes')
+  .forEach(file => {
+    let route = file.replace('.js', '')
+    app.use(`/${route}`, require(`./routes/${route}`))
+    console.log('Loaded route ' + route)
+  })
 
-// catch 404 and forward to error handler
+// catches 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found')
   err.status = 404
@@ -41,11 +44,11 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+  // sets locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
+  // renders the error page
   res.status(err.status || 500)
   res.render('error')
 })
