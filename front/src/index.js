b8faf00693e55c0892d977710314191293d1d5bb
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2017-09-17 14:38:44 -0500

    added chatbox
    not persistent yet

--- front/src/index.js
+++ front/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
 
 import { HashRouter } from 'react-router-dom'
+global.jQuery = require('jquery');
+require('bootstrap');
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.css';
 import App from './App'
 
 ReactDOM.render((
