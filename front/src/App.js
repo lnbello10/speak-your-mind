9bd71584674d531b19c3882fe514b7313e3a7352
jdguzmans <jdguzmans@hotmail.com>
2017-09-17 15:28:10 -0500

    Revert "added chatbox"
    This reverts commit b8faf00693e55c0892d977710314191293d1d5bb.

--- front/src/App.js
+++ front/src/App.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react'
-import Post from './Post/component/Post'
 import Navbar from './Navbar'
 import Main from './Main'
-import './App.css';
 
 class App extends Component {
   constructor (props) {
     super(props)
-    this.addPost = this.addPost.bind(this);
-  	this.handlePostEditorInputChange = this.handlePostEditorInputChange.bind(this);
     this.state = {
-      token: '',
-      posts:[],
- 	newPostBody:'',
+      token: ''
     }
   }
-
-
-addPost(){
-		const newState = Objet.assign(newState, this.state);
-		newState.posts.push(this.state.newPostBody);
-		newState.newPostBody ='';
-		this.setState(newState);
-}
-
-handlePostEditorInputChange(ev){
-	this.setState({
-	 newPostBody.ev.targe.value
-})
-}
 
   saveToken (token) {
     this.setState({token: token})
@@ -40,25 +20,6 @@ handlePostEditorInputChange(ev){
         <Navbar />
         <Main token={this.state.token} saveToken={this.saveToken.bind(this)} />
       </div>
-
-      <div>
-      	{this.state.posts.map((postBody, idx )=> {
-      		return(<Post key= {idx} postbody = {postBody} />)
-      			})
-      	}
-      	
-        <Navbar/>
-        <Main />
-        
-       <div className="panel panel-default post-editor">
-      		<div className = "panel-body">
-      			<textarea className= "form-contro post-editor-input" value={this.state.newPostBody} onChange={this.handlePostEditorInputChange} />
-      				<button className= "btn btn-success post-editor-button" onClick={this.addPost}>Post
-      				</button>     		 	
-      		</div>
-      	</div>
-
-      </div> 
     )
   }
 }
--- front/src/index.js
+++ front/src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
 
 import { HashRouter } from 'react-router-dom'
-global.jQuery = require('jquery');
-require('bootstrap');
-import 'bootstrap/dist/js/bootstrap.js';
-import 'bootstrap/dist/js/bootstrap.css';
 import App from './App'
 
 ReactDOM.render((
