ccb1f817cd5c588169b01aefd4c30f259f5d4fbb
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2017-09-18 02:09:16 -0500

    added content to home

--- front/src/Home.js
+++ front/src/Home.js
@@ -4,33 +4,43 @@ import React, {Component} from 'react'
 
 class Home extends Component {
   render () {
+
+  const containerStyle = {
+    textAlign: 'center'
+  }
+
     return (
       <div>
         <div className='jumbotron'>
-          <h1>Speak Your Mind</h1>
+          <h1>Speak Your Mind </h1>
           <p>Shortening distances through the world wide web.
-          We are an open multilingual comunity of people who discuss dayly themes and about anything one can come in mind.
-          The meaning is not entirely our focus, but the media.</p>
+          We are an open multilingual community of people who discuss daily topics and about anything one can come in mind.
+          The meaning is not entirely our focus, but the media. You can start by browsing some of the lobbys available or 
+          add a comment to a conversation you like by <a href='#login'>logging in</a> or <a href='#signUp'>signing up</a> for free!</p>
         </div>
-        <p>
-          Praesent faucibus interdum porta. Aliquam erat volutpat. Donec ornare lectus vitae tellus viverra egestas. In ut metus sem.
-          Duis et pharetra risus. Morbi tempor malesuada sapien, in aliquam nunc efficitur ut. Proin fermentum tristique magna vel blandit.
-          Integer egestas, mauris nec rhoncus interdum, massa nisl dapibus urna, eu porta justo odio vitae libero. Vivamus vitae leo blandit,
-          stristique ex vitae, consequat erat.
-        </p>
+
         <blockquote>
           <p>To have another language is to possess a second soul.</p>
           <small><cite title='Source Title'>Charlemagne</cite></small>
         </blockquote>
-        <p>
-          Nulla et justo justo. Suspendisse non pharetra nisi, id ultrices sapien. Nullam non rhoncus sapien, in consectetur nibh. Nunc vulputate
-          diam turpis, vel placerat leo feugiat tempus. Etiam vitae orci eget tortor aliquet vulputate. Integer convallis enim in nibh condimentum
-          accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In aliquet interdum enim, iaculis
-          molestie enim tempor at. Donec porta, ipsum vel eleifend dapibus, massa ipsum maximus ante, et hendrerit massa felis euismod nibh. Etiam
-          et scelerisque quam. Ut in ligula non justo blandit sodales sed molestie ipsum. Proin mollis at tortor sed feugiat. Curabitur facilisis
-          ex sapien, sed commodo velit malesuada quis. Fusce enim tortor, ornare ut odio vitae, auctor porta nibh. Vestibulum sed blandit elit, a
-          mollis orci. Fusce nec lectus commodo, vestibulum est sed, mattis enim.
-      </p>
+
+        <h2 style={containerStyle}> What would you like to do?</h2>      
+            <div>
+              <h3>-Start writting in:</h3>
+              <div className="row">
+                <div className = "jumbotron col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                 <h2> <a href='#chats/english'>English</a> </h2>
+                </div>
+                <div className="jumbotron col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                 <h2><a href='#chats/spanish'>Spanish</a></h2> 
+                </div>
+                <div className="jumbotron col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                  <h2><a href='#chats/french'>French</a></h2>
+                </div>
+              </div>
+            </div>
+        <h3>-Enter a <a href='#chats/english'>random</a> chat </h3>
+       <h3>-Most active lobbies:</h3>
       </div>
     )
   }
