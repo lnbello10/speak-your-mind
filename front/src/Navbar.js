f33234f9e3528d6e116f702a613710de941366ad
jdguzmans <jdguzmans@hotmail.com>
2017-09-18 22:03:59 -0500

    fixed a border

--- front/src/Navbar.js
+++ front/src/Navbar.js
@@ -5,7 +5,7 @@ import React, {Component} from 'react'
 class Navbar extends Component {
   render () {
     return (
-      <nav className='navbar navbar-inverse'>
+      <nav className='navbar navbar-inverse navbar-fixed-top'>
         <div className='container-fluid'>
           <div className='navbar-header'>
             <a className='navbar-brand' href='#home'>SpeakYourMind</a>
